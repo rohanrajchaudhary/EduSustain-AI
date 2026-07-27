8a77b3e9e56ebbae67d1561cda773eeda8af49ff
Rohan Raj Chaudhary <rohanchaudhary041999@gmail.com>
2026-07-27 16:12:09 +0530

    Fix User model filename case

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
 
-const User = require("../models/User");
+const User = require("../models/user");
 
 const router = express.Router();
 
