b6ee32853da2e1b26017b7025bf26ca36091617e
Rohan Raj Chaudhary <rohanchaudhary041999@gmail.com>
2026-07-27 16:06:02 +0530

    Fix User model import for Render

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -2,7 +2,7 @@ const passport = require("passport");
 const GoogleStrategy =
   require("passport-google-oauth20").Strategy;
 
-const User = require("../models/User");
+const User = require("../models/user");
 
 
 // =====================================================
